9681bc0938f7d8bde77b99b4d2dd4630e094425d
Chedered <gwched@gmail.com>
2021-03-17 22:27:12 +0300

    final

--- module_0/Угадай число.py
+++ module_0/Угадай число.py
@@ -1,33 +1,37 @@
 import numpy as np
-number = np.random.randint(1,101) 
-print ("Загадано число от 1 до 100")
 
 
 def game_core_v5(number):
+    """Ищем загадонное число через бинарный поиск.
+
+    Определяем середину нашего диапазона чисел и сравниваем ее с нужныи числом.
+    В зависимости от того, больше она или меньше нужного числа,
+    дальнейший поиск осуществляется в нижней или в верхней половине нашего диапазона.
+    Функция принимает загаданное число и возвращает число попыток'
+    """
     count = 0
-    predict = 50 
-    array = [25,10,5,4,3,2,1]
-    while number != predict:
-        for i in array:
-            count += 1
-            if number >= predict+i: 
-                predict += i
-                if number == predict:
-                    break
-            elif number <= predict-i: 
-                predict -= i
-                if number == predict:
-                    break
-    return(count)
+    low = 1
+    high = 100
+    mid = 0
+    while number != mid:
+        count += 1
+        mid = (low + high) // 2
+        if number < mid:
+            high = mid - 1  # прибавляем\убаляем 1, так как серидина уже сравнивалась
+        elif number > mid:
+            low = mid + 1
+    return count  # выход из цикла, если угадали
+
 
 def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+    """Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число"""
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score}({np.mean(count_ls)}) попыток")
+
 
 score_game(game_core_v5)
